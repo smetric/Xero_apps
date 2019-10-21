00797aaa28db0f8cfbe624139071e962b3b8b03a
Nayana <data@smetric.co.nz>
2019-10-21 18:02:31 +1300

    Fix: Update permissions for Geckodriver in /usr/bin/geckodriver

--- main.py
+++ main.py
@@ -431,7 +431,7 @@ def main(params, datadir = '/data/', download_dir = '/tmp/downloads/', headless=
             
 #            advisor_pagination_url = advisor_data['advisor_page']
             for k in range(1, page_number):
-                wd.driver.manage().deleteAllCookies()
+                wd.driver.delete_all_cookies()
                 apps_df = get_advisor_apps(wd, advisor_df, location, k, Current_page_url)
 #                xero_apps_df = xero_apps_df.append(apps_df, ignore_index = True)
                 write_csv(df = apps_df, fp = Path(outdir) / (filename + '.csv'))
